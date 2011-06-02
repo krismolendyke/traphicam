35cc8688aaa7db93b9c8ecd625d39f22753193a2
Kris Molendyke <kris@visibiz.com>
2011-06-01 20:19:10 -0400

    Removed more unused files.

--- prod/src/shell/curlcams.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-#url=http://www.dot35.state.pa.us/public/Districts/District6/WebCams/
-url=http://www.dot35.state.pa.us/public/Districts/District5/WebCams/
-out=test.txt
-
-curl "${url}D5cam[001-100].jpg" --silent -o 'AAA.jpg' -# -w "%{url_effective} %{http_code}\n" > ${out}
